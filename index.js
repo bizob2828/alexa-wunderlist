6e8b0d086778c6e987d857e709d7be2a103ac972
Bob Evans <robert.evans25@gmail.com>
2017-03-08 16:28:25 -0500

    finished skill

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 'use strict';
+const Alexa = require('alexa-sdk');
 const request = require('request');
 const _ = require('lodash');
-const Promise = require('bluebird');
-const req = Promise.promisify(request);
+const promise = require('bluebird');
+const req = promise.promisify(request);
 const apiBase = 'https://a.wunderlist.com/api/v1';
-
 
 function makeRequest(uri, method, data) {
   let opts = {
@@ -27,7 +27,7 @@ function makeRequest(uri, method, data) {
 
 function getLists() {
   return makeRequest('lists').then((lists) => {
-    return _.map(JSON.parse(lists), ({ id, title}) => ({ id, title }));
+    return _.map(JSON.parse(lists), (list) => ({ id: list.id, title: list.title }));
   });
 }
 
@@ -38,6 +38,15 @@ function getList(list) {
 
 }
 
+function getTasks(list) {
+  return getList(list).then((id) => {
+    return makeRequest(`tasks?list_id=${id}`, 'GET');
+  })
+  .then((tasks) => {
+    return _.map(JSON.parse(tasks), (list) => ({ id: list.id, title: list.title }));
+  });
+}
+
 function createTask(task, list) {
   return getList(list).then((id) => {
     return makeRequest('tasks', 'POST', { list_id: id, title: task });
@@ -45,3 +54,29 @@ function createTask(task, list) {
 
 }
 
+const handlers = {
+  'AddItem': function() {
+    let list = this.event.request.intent.slots.List.value
+      , item = this.event.request.intent.slots.Item.value;
+
+    createTask(item, list).then(() => this.emit(':tell', `Item added to ${list}`));
+  },
+  'ListContents': function() {
+    let list = this.event.request.intent.slots.List.value;
+
+    getTasks(list).then((tasks) => {
+      this.emit(':tell', `Items on list are: ${_.map(tasks, 'title').join(' ')}`);
+    });
+  },
+  'GetLists': function() {
+    getLists().then((lists) => {
+      this.emit(':tell', `Your lists are : ${_.map(lists, 'title').join(' ')}`);
+    });
+  }
+};
+
+exports.handler = (event, context, cb) => {
+  const alexa = Alexa.handler(event, context);
+  alexa.registerHandlers(handlers);
+  alexa.execute();
+};
