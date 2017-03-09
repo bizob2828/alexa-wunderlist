568a2c492400eebb07e400878b6182b3c44c9954
Bob Evans <robert.evans25@gmail.com>
2017-03-09 17:01:20 -0500

    added some npm scripts

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ const promise = require('bluebird');
 const req = promise.promisify(request);
 const apiBase = 'https://a.wunderlist.com/api/v1';
 
+/**
+ * Convenience method to make http requests to wunderlist API
+ * @param uri {String} uri to make http request
+ * @param method {String} POST, PUT, DELETE, defaults to GET
+ * @param data {Object} post data for POST/PUT/DELETE requests
+ */
 function makeRequest(uri, method, data) {
   let opts = {
     url: `${apiBase}/${uri}`,
@@ -25,12 +31,21 @@ function makeRequest(uri, method, data) {
   return req(opts).then((res) => res.body);
 }
 
+/**
+ * Retrieve all wunderlists
+ * @return array of objects with id, title
+ */
 function getLists() {
   return makeRequest('lists').then((lists) => {
     return _.map(JSON.parse(lists), (list) => ({ id: list.id, title: list.title }));
   });
 }
 
+/**
+ * Find a list by title
+ * @param list {String} title of list
+ * @return {Object} of entire list details
+ */
 function getList(list) {
   return makeRequest('lists').then((lists) => {
     return _.filter(JSON.parse(lists), (l) => l.title.toLowerCase() === list.toLowerCase())[0].id;
@@ -38,6 +53,11 @@ function getList(list) {
 
 }
 
+/**
+ * Retrieve tasks for a list
+ * @param list {String} title of list
+ * @returns array of objects with id, title
+ */
 function getTasks(list) {
   return getList(list).then((id) => {
     return makeRequest(`tasks?list_id=${id}`, 'GET');
@@ -47,6 +67,11 @@ function getTasks(list) {
   });
 }
 
+/**
+ * Creates a task for a list
+ * @param task {String} name of task
+ * @param list {String} title of list
+ */
 function createTask(task, list) {
   return getList(list).then((id) => {
     return makeRequest('tasks', 'POST', { list_id: id, title: task });
@@ -54,12 +79,13 @@ function createTask(task, list) {
 
 }
 
+// defines how to handle the alexa skill methods
 const handlers = {
   'AddItem': function() {
     let list = this.event.request.intent.slots.List.value
       , item = this.event.request.intent.slots.Item.value;
 
-    createTask(item, list).then(() => this.emit(':tell', `Item added to ${list}`));
+    createTask(item, list).then(() => this.emit(':tell', `Item ${item} added to ${list}`));
   },
   'ListContents': function() {
     let list = this.event.request.intent.slots.List.value;
@@ -75,6 +101,7 @@ const handlers = {
   }
 };
 
+// registers handlers with alexa sdk
 exports.handler = (event, context, cb) => {
   const alexa = Alexa.handler(event, context);
   alexa.registerHandlers(handlers);
